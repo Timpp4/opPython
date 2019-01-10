7b6124b6182b7dba5003b7c10222aaf81f706a59
Timpp4 <38942076+Timpp4@users.noreply.github.com>
2019-01-10 08:50:26 +0200

    Add files via upload

---
+++ verolib.py
@@ -0,0 +1,814 @@
+# CT60A0201 Ohjelmoinnin perusteet 2017
+# Tekijä: Tuomas Ikonen
+# Opiskelijanumero: 
+# Päivämäärä: 14.11.2017
+# Yhteistyö: Moodle, LUT-ohjelmointiopas, Google
+# Harjoitustyö 1
+######################################################################
+
+#20171019 CO2vero.py un
+def vero(x):
+    if (x == 0):
+        return (106.21)
+    elif (x == 1):
+        return (106.58)
+    elif (x == 2):
+        return (106.94)
+    elif (x == 3):
+        return (106.94)
+    elif (x == 4):
+        return (107.31)
+    elif (x == 5):
+        return (107.67)
+    elif (x == 6):
+        return (108.04)
+    elif (x == 7):
+        return (108.40)
+    elif (x == 8):
+        return (108.77)
+    elif (x == 9):
+        return (108.77)
+    elif (x == 10):
+        return (109.13)
+    elif (x == 11):
+        return (109.50)
+    elif (x == 12):
+        return (109.86)
+    elif (x == 13):
+        return (110.23)
+    elif (x == 14):
+        return (110.59)
+    elif (x == 15):
+        return (110.96)
+    elif (x == 16):
+        return (111.32)
+    elif (x == 17):
+        return (111.69)
+    elif (x == 18):
+        return (112.05)
+    elif (x == 19):
+        return (112.42)
+    elif (x == 20):
+        return (112.78)
+    elif (x == 21):
+        return (113.15)
+    elif (x == 22):
+        return (113.51)
+    elif (x == 23):
+        return (113.88)
+    elif (x == 24):
+        return (114.24)
+    elif (x == 25):
+        return (114.61)
+    elif (x == 26):
+        return (114.97)
+    elif (x == 27):
+        return (115.34)
+    elif (x == 28):
+        return (115.70)
+    elif (x == 29):
+        return (116.07)
+    elif (x == 30):
+        return (116.80)
+    elif (x == 31):
+        return (117.16)
+    elif (x == 32):
+        return (117.53)
+    elif (x == 33):
+        return (117.89)
+    elif (x == 34):
+        return (118.26)
+    elif (x == 35):
+        return (118.62)
+    elif (x == 36):
+        return (119.35)
+    elif (x == 37):
+        return (119.72)
+    elif (x == 38):
+        return (120.08)
+    elif (x == 39):
+        return (120.45)
+    elif (x == 40):
+        return (121.18)
+    elif (x == 41):
+        return (121.54)
+    elif (x == 42):
+        return (121.91)
+    elif (x == 43):
+        return (122.64)
+    elif (x == 44):
+        return (123.00)
+    elif (x == 45):
+        return (123.37)
+    elif (x == 46):
+        return (124.10)
+    elif (x == 47):
+        return (124.46)
+    elif (x == 48):
+        return (125.19)
+    elif (x == 49):
+        return (125.56)
+    elif (x == 50):
+        return (126.29)
+    elif (x == 51):
+        return (126.65)
+    elif (x == 52):
+        return (127.38)
+    elif (x == 53):
+        return (127.75)
+    elif (x == 54):
+        return (128.48)
+    elif (x == 55):
+        return (128.84)
+    elif (x == 56):
+        return (129.57)
+    elif (x == 57):
+        return (129.94)
+    elif (x == 58):
+        return (130.67)
+    elif (x == 59):
+        return (131.40)
+    elif (x == 60):
+        return (131.76)
+    elif (x == 61):
+        return (132.49)
+    elif (x == 62):
+        return (133.22)
+    elif (x == 63):
+        return (133.59)
+    elif (x == 64):
+        return (134.32)
+    elif (x == 65):
+        return (135.05)
+    elif (x == 66):
+        return (135.78)
+    elif (x == 67):
+        return (136.14)
+    elif (x == 68):
+        return (136.87)
+    elif (x == 69):
+        return (137.60)
+    elif (x == 70):
+        return (138.33)
+    elif (x == 71):
+        return (139.06)
+    elif (x == 72):
+        return (139.79)
+    elif (x == 73):
+        return (140.52)
+    elif (x == 74):
+        return (141.25)
+    elif (x == 75):
+        return (141.98)
+    elif (x == 76):
+        return (142.71)
+    elif (x == 77):
+        return (143.44)
+    elif (x == 78):
+        return (144.17)
+    elif (x == 79):
+        return (144.90)
+    elif (x == 80):
+        return (145.63)
+    elif (x == 81):
+        return (146.36)
+    elif (x == 82):
+        return (147.09)
+    elif (x == 83):
+        return (147.82)
+    elif (x == 84):
+        return (148.92)
+    elif (x == 85):
+        return (149.65)
+    elif (x == 86):
+        return (150.38)
+    elif (x == 87):
+        return (151.11)
+    elif (x == 88):
+        return (152.20)
+    elif (x == 89):
+        return (152.93)
+    elif (x == 90):
+        return (153.66)
+    elif (x == 91):
+        return (154.76)
+    elif (x == 92):
+        return (155.49)
+    elif (x == 93):
+        return (156.58)
+    elif (x == 94):
+        return (157.31)
+    elif (x == 95):
+        return (158.04)
+    elif (x == 96):
+        return (159.14)
+    elif (x == 97):
+        return (160.23)
+    elif (x == 98):
+        return (160.96)
+    elif (x == 99):
+        return (162.06)
+    elif (x == 100):
+        return (162.79)
+    elif (x == 101):
+        return (163.88)
+    elif (x == 102):
+        return (164.98)
+    elif (x == 103):
+        return (165.71)
+    elif (x == 104):
+        return (166.80)
+    elif (x == 105):
+        return (167.90)
+    elif (x == 106):
+        return (168.99)
+    elif (x == 107):
+        return (169.72)
+    elif (x == 108):
+        return (170.82)
+    elif (x == 109):
+        return (171.91)
+    elif (x == 110):
+        return (173.01)
+    elif (x == 111):
+        return (174.10)
+    elif (x == 112):
+        return (175.20)
+    elif (x == 113):
+        return (176.29)
+    elif (x == 114):
+        return (177.39)
+    elif (x == 115):
+        return (178.48)
+    elif (x == 116):
+        return (179.58)
+    elif (x == 117):
+        return (180.67)
+    elif (x == 118):
+        return (182.13)
+    elif (x == 119):
+        return (183.23)
+    elif (x == 120):
+        return (184.32)
+    elif (x == 121):
+        return (185.42)
+    elif (x == 122):
+        return (186.51)
+    elif (x == 123):
+        return (187.97)
+    elif (x == 124):
+        return (189.07)
+    elif (x == 125):
+        return (190.16)
+    elif (x == 126):
+        return (191.62)
+    elif (x == 127):
+        return (192.72)
+    elif (x == 128):
+        return (194.18)
+    elif (x == 129):
+        return (195.27)
+    elif (x == 130):
+        return (196.73)
+    elif (x == 131):
+        return (197.83)
+    elif (x == 132):
+        return (199.29)
+    elif (x == 133):
+        return (200.75)
+    elif (x == 134):
+        return (201.84)
+    elif (x == 135):
+        return (203.30)
+    elif (x == 136):
+        return (204.76)
+    elif (x == 137):
+        return (206.22)
+    elif (x == 138):
+        return (207.32)
+    elif (x == 139):
+        return (208.78)
+    elif (x == 140):
+        return (210.24)
+    elif (x == 141):
+        return (211.70)
+    elif (x == 142):
+        return (213.16)
+    elif (x == 143):
+        return (214.62)
+    elif (x == 144):
+        return (216.08)
+    elif (x == 145):
+        return (217.54)
+    elif (x == 146):
+        return (219.00)
+    elif (x == 147):
+        return (220.46)
+    elif (x == 148):
+        return (221.92)
+    elif (x == 149):
+        return (223.38)
+    elif (x == 150):
+        return (225.20)
+    elif (x == 151):
+        return (226.66)
+    elif (x == 152):
+        return (228.12)
+    elif (x == 153):
+        return (229.58)
+    elif (x == 154):
+        return (231.41)
+    elif (x == 155):
+        return (232.87)
+    elif (x == 156):
+        return (234.33)
+    elif (x == 157):
+        return (236.15)
+    elif (x == 158):
+        return (237.61)
+    elif (x == 159):
+        return (239.44)
+    elif (x == 160):
+        return (240.90)
+    elif (x == 161):
+        return (242.72)
+    elif (x == 162):
+        return (244.55)
+    elif (x == 163):
+        return (246.01)
+    elif (x == 164):
+        return (247.83)
+    elif (x == 165):
+        return (249.29)
+    elif (x == 166):
+        return (251.12)
+    elif (x == 167):
+        return (252.94)
+    elif (x == 168):
+        return (254.77)
+    elif (x == 169):
+        return (256.59)
+    elif (x == 170):
+        return (258.05)
+    elif (x == 171):
+        return (259.88)
+    elif (x == 172):
+        return (261.70)
+    elif (x == 173):
+        return (263.53)
+    elif (x == 174):
+        return (265.35)
+    elif (x == 175):
+        return (267.18)
+    elif (x == 176):
+        return (269.00)
+    elif (x == 177):
+        return (270.83)
+    elif (x == 178):
+        return (272.65)
+    elif (x == 179):
+        return (274.48)
+    elif (x == 180):
+        return (276.67)
+    elif (x == 181):
+        return (278.49)
+    elif (x == 182):
+        return (280.32)
+    elif (x == 183):
+        return (282.14)
+    elif (x == 184):
+        return (283.97)
+    elif (x == 185):
+        return (286.16)
+    elif (x == 186):
+        return (287.98)
+    elif (x == 187):
+        return (289.81)
+    elif (x == 188):
+        return (292.00)
+    elif (x == 189):
+        return (293.82)
+    elif (x == 190):
+        return (296.01)
+    elif (x == 191):
+        return (297.84)
+    elif (x == 192):
+        return (299.66)
+    elif (x == 193):
+        return (301.85)
+    elif (x == 194):
+        return (304.04)
+    elif (x == 195):
+        return (305.87)
+    elif (x == 196):
+        return (308.06)
+    elif (x == 197):
+        return (309.88)
+    elif (x == 198):
+        return (312.07)
+    elif (x == 199):
+        return (313.90)
+    elif (x == 200):
+        return (316.09)
+    elif (x == 201):
+        return (318.28)
+    elif (x == 202):
+        return (320.10)
+    elif (x == 203):
+        return (322.29)
+    elif (x == 204):
+        return (324.48)
+    elif (x == 205):
+        return (326.67)
+    elif (x == 206):
+        return (328.50)
+    elif (x == 207):
+        return (330.69)
+    elif (x == 208):
+        return (332.88)
+    elif (x == 209):
+        return (335.07)
+    elif (x == 210):
+        return (337.26)
+    elif (x == 211):
+        return (339.45)
+    elif (x == 212):
+        return (341.27)
+    elif (x == 213):
+        return (343.46)
+    elif (x == 214):
+        return (345.65)
+    elif (x == 215):
+        return (347.84)
+    elif (x == 216):
+        return (350.03)
+    elif (x == 217):
+        return (352.22)
+    elif (x == 218):
+        return (354.41)
+    elif (x == 219):
+        return (356.60)
+    elif (x == 220):
+        return (358.79)
+    elif (x == 221):
+        return (360.98)
+    elif (x == 222):
+        return (363.17)
+    elif (x == 223):
+        return (365.36)
+    elif (x == 224):
+        return (367.55)
+    elif (x == 225):
+        return (369.74)
+    elif (x == 226):
+        return (371.93)
+    elif (x == 227):
+        return (374.12)
+    elif (x == 228):
+        return (376.31)
+    elif (x == 229):
+        return (378.50)
+    elif (x == 230):
+        return (380.69)
+    elif (x == 231):
+        return (382.88)
+    elif (x == 232):
+        return (385.07)
+    elif (x == 233):
+        return (387.63)
+    elif (x == 234):
+        return (389.82)
+    elif (x == 235):
+        return (392.01)
+    elif (x == 236):
+        return (394.20)
+    elif (x == 237):
+        return (396.39)
+    elif (x == 238):
+        return (398.58)
+    elif (x == 239):
+        return (400.77)
+    elif (x == 240):
+        return (402.96)
+    elif (x == 241):
+        return (405.15)
+    elif (x == 242):
+        return (407.34)
+    elif (x == 243):
+        return (409.53)
+    elif (x == 244):
+        return (412.08)
+    elif (x == 245):
+        return (414.27)
+    elif (x == 246):
+        return (416.46)
+    elif (x == 247):
+        return (418.65)
+    elif (x == 248):
+        return (420.84)
+    elif (x == 249):
+        return (423.03)
+    elif (x == 250):
+        return (425.22)
+    elif (x == 251):
+        return (427.41)
+    elif (x == 252):
+        return (429.60)
+    elif (x == 253):
+        return (431.79)
+    elif (x == 254):
+        return (433.98)
+    elif (x == 255):
+        return (436.17)
+    elif (x == 256):
+        return (438.36)
+    elif (x == 257):
+        return (440.55)
+    elif (x == 258):
+        return (442.74)
+    elif (x == 259):
+        return (444.93)
+    elif (x == 260):
+        return (447.12)
+    elif (x == 261):
+        return (449.31)
+    elif (x == 262):
+        return (451.50)
+    elif (x == 263):
+        return (453.69)
+    elif (x == 264):
+        return (455.88)
+    elif (x == 265):
+        return (457.71)
+    elif (x == 266):
+        return (459.90)
+    elif (x == 267):
+        return (462.09)
+    elif (x == 268):
+        return (464.28)
+    elif (x == 269):
+        return (466.47)
+    elif (x == 270):
+        return (468.66)
+    elif (x == 271):
+        return (470.48)
+    elif (x == 272):
+        return (472.67)
+    elif (x == 273):
+        return (474.86)
+    elif (x == 274):
+        return (477.05)
+    elif (x == 275):
+        return (478.88)
+    elif (x == 276):
+        return (481.07)
+    elif (x == 277):
+        return (483.26)
+    elif (x == 278):
+        return (485.08)
+    elif (x == 279):
+        return (487.27)
+    elif (x == 280):
+        return (489.10)
+    elif (x == 281):
+        return (491.29)
+    elif (x == 282):
+        return (493.11)
+    elif (x == 283):
+        return (495.30)
+    elif (x == 284):
+        return (497.49)
+    elif (x == 285):
+        return (499.32)
+    elif (x == 286):
+        return (501.14)
+    elif (x == 287):
+        return (503.33)
+    elif (x == 288):
+        return (505.16)
+    elif (x == 289):
+        return (507.35)
+    elif (x == 290):
+        return (509.17)
+    elif (x == 291):
+        return (511.00)
+    elif (x == 292):
+        return (513.19)
+    elif (x == 293):
+        return (515.01)
+    elif (x == 294):
+        return (516.84)
+    elif (x == 295):
+        return (518.66)
+    elif (x == 296):
+        return (520.49)
+    elif (x == 297):
+        return (522.68)
+    elif (x == 298):
+        return (524.50)
+    elif (x == 299):
+        return (526.33)
+    elif (x == 300):
+        return (528.15)
+    elif (x == 301):
+        return (529.98)
+    elif (x == 302):
+        return (531.80)
+    elif (x == 303):
+        return (533.63)
+    elif (x == 304):
+        return (535.45)
+    elif (x == 305):
+        return (537.28)
+    elif (x == 306):
+        return (539.10)
+    elif (x == 307):
+        return (540.56)
+    elif (x == 308):
+        return (542.39)
+    elif (x == 309):
+        return (544.21)
+    elif (x == 310):
+        return (546.04)
+    elif (x == 311):
+        return (547.86)
+    elif (x == 312):
+        return (549.32)
+    elif (x == 313):
+        return (551.15)
+    elif (x == 314):
+        return (552.61)
+    elif (x == 315):
+        return (554.43)
+    elif (x == 316):
+        return (556.26)
+    elif (x == 317):
+        return (557.72)
+    elif (x == 318):
+        return (559.54)
+    elif (x == 319):
+        return (561.00)
+    elif (x == 320):
+        return (562.83)
+    elif (x == 321):
+        return (564.29)
+    elif (x == 322):
+        return (565.75)
+    elif (x == 323):
+        return (567.57)
+    elif (x == 324):
+        return (569.03)
+    elif (x == 325):
+        return (570.49)
+    elif (x == 326):
+        return (571.95)
+    elif (x == 327):
+        return (573.78)
+    elif (x == 328):
+        return (575.24)
+    elif (x == 329):
+        return (576.70)
+    elif (x == 330):
+        return (578.16)
+    elif (x == 331):
+        return (579.62)
+    elif (x == 332):
+        return (581.08)
+    elif (x == 333):
+        return (582.54)
+    elif (x == 334):
+        return (584.00)
+    elif (x == 335):
+        return (585.46)
+    elif (x == 336):
+        return (586.92)
+    elif (x == 337):
+        return (588.38)
+    elif (x == 338):
+        return (589.84)
+    elif (x == 339):
+        return (590.93)
+    elif (x == 340):
+        return (592.39)
+    elif (x == 341):
+        return (593.85)
+    elif (x == 342):
+        return (595.31)
+    elif (x == 343):
+        return (596.41)
+    elif (x == 344):
+        return (597.87)
+    elif (x == 345):
+        return (599.33)
+    elif (x == 346):
+        return (600.42)
+    elif (x == 347):
+        return (601.88)
+    elif (x == 348):
+        return (602.98)
+    elif (x == 349):
+        return (604.44)
+    elif (x == 350):
+        return (605.53)
+    elif (x == 351):
+        return (606.99)
+    elif (x == 352):
+        return (608.09)
+    elif (x == 353):
+        return (609.18)
+    elif (x == 354):
+        return (610.64)
+    elif (x == 355):
+        return (611.74)
+    elif (x == 356):
+        return (612.83)
+    elif (x == 357):
+        return (613.93)
+    elif (x == 358):
+        return (615.02)
+    elif (x == 359):
+        return (616.48)
+    elif (x == 360):
+        return (617.58)
+    elif (x == 361):
+        return (618.67)
+    elif (x == 362):
+        return (619.77)
+    elif (x == 363):
+        return (620.86)
+    elif (x == 364):
+        return (621.96)
+    elif (x == 365):
+        return (623.05)
+    elif (x == 366):
+        return (624.15)
+    elif (x == 367):
+        return (625.24)
+    elif (x == 368):
+        return (626.34)
+    elif (x == 369):
+        return (627.43)
+    elif (x == 370):
+        return (628.16)
+    elif (x == 371):
+        return (629.26)
+    elif (x == 372):
+        return (630.35)
+    elif (x == 373):
+        return (631.45)
+    elif (x == 374):
+        return (632.18)
+    elif (x == 375):
+        return (633.27)
+    elif (x == 376):
+        return (634.37)
+    elif (x == 377):
+        return (635.10)
+    elif (x == 378):
+        return (636.19)
+    elif (x == 379):
+        return (636.92)
+    elif (x == 380):
+        return (638.02)
+    elif (x == 381):
+        return (639.11)
+    elif (x == 382):
+        return (639.84)
+    elif (x == 383):
+        return (640.57)
+    elif (x == 384):
+        return (641.67)
+    elif (x == 385):
+        return (642.40)
+    elif (x == 386):
+        return (643.49)
+    elif (x == 387):
+        return (644.22)
+    elif (x == 388):
+        return (644.95)
+    elif (x == 389):
+        return (646.05)
+    elif (x == 390):
+        return (646.78)
+    elif (x == 391):
+        return (647.51)
+    elif (x == 392):
+        return (648.24)
+    elif (x == 393):
+        return (649.33)
+    elif (x == 394):
+        return (650.06)
+    elif (x == 395):
+        return (650.79)
+    elif (x == 396):
+        return (651.52)
+    elif (x == 397):
+        return (652.25)
+    elif (x == 398):
+        return (652.98)
+    elif (x == 399):
+        return (653.71)
+    elif (x >= 400):
+        return (654.44)
+#end of CO2vero.py un
+#eof
